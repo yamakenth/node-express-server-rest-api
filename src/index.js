ae68ae1cf37fce5ff7d4a5935c1dcb102daa6f6c
Ken Yamada <yamakenth@gmail.com>
2022-02-21 14:27:09 +0700

    Setup Express app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
-console.log('Hello Node.js project.');
-console.log(process.env.MY_SECRET);
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => 
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
