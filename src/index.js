cbd632c3a83721c7fa2bd0933943914a7273bbed
Ken Yamada <yamakenth@gmail.com>
2022-02-21 17:26:16 +0700

    Modularize routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import models from './models';
+import routes from './routes';
 
 const app = express();
 
@@ -19,54 +20,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+
 // index 
 app.get('/', (req, res) => {
   return res.send('Welcome to example app.');
-});
-
-// users 
-app.get('/users', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-// messages 
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = { 
-    id, 
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-  req.context.models.messages[id] = message;
-  return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
-});
-
-// session 
-app.get('/session', (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 app.listen(process.env.PORT, () => 
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+import session from './session';
+import user from './user';
+import message from './message';
+
+export default { session, user, message, };
